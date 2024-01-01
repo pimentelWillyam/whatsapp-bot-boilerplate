ebe93978174f647d952a851b028eedc1ec97e3df
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-01 19:19:01 -0300

    refactor: changing return logic

--- src/bot/factory/MessageListenerFactory.ts
+++ src/bot/factory/MessageListenerFactory.ts
@@ -1,10 +1,14 @@
 import { MessageListener } from '../helper/MessageListener'
 import { type IMessageListener } from '../interface/IMessageListener'
-import { type IMessageListenerFactory } from '../interface/IMessageHandlerFactory'
+import { type IMessageListenerFactory } from '../interface/IMessageListenerFactory'
+import { type WhatsappClient } from '../type/WhatsappClient'
+import { MessageHandlerFactory } from './MessageHandlerFactory'
 
 class MessageListenerFactory implements IMessageListenerFactory {
-  fabricate (): IMessageListener {
-    return new MessageListener()
+  private readonly messageHandlerFactory = new MessageHandlerFactory()
+
+  fabricate (whatsappClient: WhatsappClient): IMessageListener {
+    return new MessageListener(whatsappClient, this.messageHandlerFactory.fabricate(whatsappClient))
   }
 }
 
