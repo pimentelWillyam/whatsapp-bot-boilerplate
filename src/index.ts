c75e8fb760a879c96c8f02b5e30ad60205efe6e6
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-01 19:20:10 -0300

    refactor: adding wrapper function at entry point

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { BotFactory } from './bot/factory/BotFactory'
 import { DataSourceFactory } from './api/factory/DataSourceFactory'
 import { APIFactory } from './api/factory/APIFactory'
 
-const bot = new BotFactory().fabricate()
-const dataSource = new DataSourceFactory().fabricate('memory')
-const api = new APIFactory(dataSource).fabricate()
+const main = async (): Promise<void> => {
+  const bot = await new BotFactory().fabricate('session-name')
+  const dataSource = new DataSourceFactory().fabricate('memory')
+  const api = new APIFactory(dataSource).fabricate()
 
-void bot.start()
-dataSource.start()
-api.start(4000)
+  await bot.start()
+  dataSource.start()
+  api.start(4000)
+}
+
+void main()
