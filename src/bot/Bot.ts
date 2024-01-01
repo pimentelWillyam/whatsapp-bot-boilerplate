557bfa638758295484e68dbbd5e046da77da2e3c
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-01 18:42:09 -0300

    refactor: changing bot start and stop methods logic

--- src/bot/Bot.ts
+++ src/bot/Bot.ts
@@ -1,30 +1,16 @@
 import { type IBot } from './interface/IBot'
 
-import { create, type Whatsapp } from 'venom-bot'
 import { type IMessageListener } from './interface/IMessageListener'
 
 class Bot implements IBot {
-  private client: Whatsapp | undefined
   constructor (private readonly messageListener: IMessageListener) {}
 
-  private async bootstrap (): Promise<void> {
-    create({ session: 'whatsapp-bot ' }).then((whatsappClient) => {
-      this.client = whatsappClient
-      void this.start()
-    }).catch((error) => { console.error(error) })
-  }
-
   async start (): Promise<void> {
-    await this.bootstrap()
-    if (this.client === undefined) throw new Error('Client is undefined')
-    await this.client.onMessage((message) => {
-      void this.messageListener.listen(this.client as Whatsapp, message)
-    })
+    void this.messageListener.start()
   }
 
   async stop (): Promise<void> {
-    if (this.client === undefined) throw new Error('Error with client connection')
-    await this.client.close()
+    void this.messageListener.stop()
   }
 }
 
