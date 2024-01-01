f3ecd89ec00dd44342416f5bc5aef1dd434691b7
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-01 19:19:11 -0300

    refactor: adding start and stop methods

--- src/bot/helper/MessageListener.ts
+++ src/bot/helper/MessageListener.ts
@@ -1,12 +1,19 @@
-import type { Whatsapp, Message } from 'venom-bot'
 import { type IMessageListener } from '../interface/IMessageListener'
+import { type IMessageHandler } from '../interface/IMessageHandler'
+
+import { type WhatsappClient } from '../type/WhatsappClient'
 
 class MessageListener implements IMessageListener {
-  async listen (client: Whatsapp, message: Message): Promise<void> {
-    console.log('received message')
-    if (message.isGroupMsg) {
-      await client.sendText(message.from, 'Olá, estou ligado e funcionando!')
-    }
+  constructor (private readonly whatsappClient: WhatsappClient, private readonly messageHandler: IMessageHandler) {}
+
+  async start (): Promise<void> {
+    await this.whatsappClient.onMessage((message) => {
+      this.messageHandler.handle(message).then().catch((err) => { console.error(err) })
+    })
+  }
+
+  async stop (): Promise<void> {
+    await this.whatsappClient.close()
   }
 }
 
