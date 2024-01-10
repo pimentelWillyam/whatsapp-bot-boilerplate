a30120507b61885ca6b75167f07facb6295138cb
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-09 21:28:41 -0300

    feat: adding update method

--- src/api/controller/PersonController.ts
+++ src/api/controller/PersonController.ts
@@ -51,14 +51,15 @@ class PersonController implements IPersonController {
     }
   }
 
-  update (req: Request, res: Response): Response<any, Record<string, any>> {
+  async update (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     const errorList = this.personValidator.validateUpdate(req.body.name, req.body.email, req.body.age)
     if (errorList.length !== 0) return res.status(400).json({ errorList })
     try {
-      const person = this.personService.update(req.params.id, req.body)
+      const person = await this.personService.update(req.params.id, req.body)
       if (person !== null) return res.status(200).send(person)
       else return res.status(404).send({ name: 'Pessoa não encontrada', message: 'Não foi possível encontrar uma pessoa com este ID' })
     } catch (error) {
+      console.error(error)
       if (error instanceof KnownError) {
         return res.status(error.status).send({ name: error.name, message: error.message })
       }
