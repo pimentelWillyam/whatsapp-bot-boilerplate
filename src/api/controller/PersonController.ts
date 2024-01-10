fcdfbe3957cedb2ead0123d26f5ddd6cf45ea539
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-09 21:28:50 -0300

    feat: adding delete

--- src/api/controller/PersonController.ts
+++ src/api/controller/PersonController.ts
@@ -67,12 +67,13 @@ class PersonController implements IPersonController {
     }
   }
 
-  delete (req: Request, res: Response): Response<any, Record<string, any>> {
+  async delete (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      const person = this.personService.delete(req.params.id)
+      const person = await this.personService.delete(req.params.id)
       if (person !== null) return res.status(200).send(person)
       else return res.status(404).send({ name: 'Pessoa não encontrada', message: 'Não foi possível encontrar uma pessoa com este ID' })
     } catch (error) {
+      console.error(error)
       if (error instanceof KnownError) {
         return res.status(error.status).send({ name: error.name, message: error.message })
       }
