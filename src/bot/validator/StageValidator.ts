447a66cf8a3af07109e133fd770598978db84684
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-09 21:49:49 -0300

    feat: adding stage validator

---
+++ src/bot/validator/StageValidator.ts
@@ -0,0 +1,8 @@
+import { type Stage } from '../entity/Stage'
+import { type IStageValidator } from '../interface/IStageValidator'
+
+class StageValidator implements IStageValidator {
+  validate (stage: Stage): void {
+    
+  }
+}
