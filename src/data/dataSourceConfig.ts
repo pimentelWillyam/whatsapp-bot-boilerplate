4f778ab3098308eb9de214dee0ab4b684fd1c9e2
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-09 21:49:57 -0300

    feat: adding datasourceconfig file

---
+++ src/data/dataSourceConfig.ts
@@ -0,0 +1,11 @@
+const dataSourceConfig = {
+  mariadb: {
+    host: 'localhost',
+    port: 3306,
+    userName: 'root',
+    password: 'mariadb',
+    connectionLimit: 5
+  }
+}
+
+export { dataSourceConfig }
