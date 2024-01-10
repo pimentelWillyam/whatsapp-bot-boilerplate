d8b6bbdc7cf17f74368261fdae570ab87cd65359
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-01-09 21:52:27 -0300

    refactor: adding promises to memory datasource

--- src/data/MemoryDataSource.ts
+++ src/data/MemoryDataSource.ts
@@ -1,53 +1,54 @@
 import type Person from '../api/entity/Person'
-import { type IMemoryDataSource } from '../api/interface/IMemoryDataSource'
+import { type IMemoryDataSource } from './interface/IMemoryDataSource'
+import { type DatabasePerson } from './model/DatabasePerson'
 
 class MemoryDataSource implements IMemoryDataSource {
   private personList: Person[] = []
 
-  readonly start = (): void => {
-    this.createPersonTable()
+  readonly start = async (): Promise<void> => {
+    await this.createPersonTable()
   }
 
-  readonly stop = (): void => {
-    this.createPersonTable()
+  readonly stop = async (): Promise<void> => {
+    await this.createPersonTable()
   }
 
-  readonly createPersonTable = (): void => {
+  readonly createPersonTable = async (): Promise<void> => {
     this.personList = []
   }
 
-  readonly dropPersonTable = (): void => {
+  readonly dropPersonTable = async (): Promise<void> => {
     this.personList = []
   }
 
-  insertPersonRegistry = (id: string, name: string, email: string, age: number): Person => {
-    this.personList.push({ id, name, email, age })
-    return { id, name, email, age }
+  insertPersonRegistry = async (person: Person): Promise<DatabasePerson> => {
+    this.personList.push(person)
+    return person
   }
 
-  fetchEveryPersonRegistry = (): Person[] => {
+  fetchEveryPersonRegistry = async (): Promise<DatabasePerson[]> => {
     return this.personList
   }
 
-  fetchPersonRegistry = (id: string): Person | null => {
+  fetchPersonBy = async (parameter: string, parameterValue: string): Promise<DatabasePerson | null> => {
     for (const person of this.personList) {
-      if (person.id === id) return person
+      if (parameter === 'id') if (person.id === parameterValue) return person
     }
     return null
   }
 
-  updatePersonRegistry = (id: string, personToBeUpdated: Person): Person | null => {
-    const person = this.fetchPersonRegistry(id)
+  updatePersonBy = async (parameter: string, parameterValue: string, personToBeUpdated: Person): Promise<DatabasePerson | null> => {
+    const person = await this.fetchPersonBy('id', parameterValue)
     if (person === null) return null
-    if (personToBeUpdated.name != undefined) person.name = personToBeUpdated.name
-    if (personToBeUpdated.email != undefined) person.email = personToBeUpdated.email
-    if (personToBeUpdated.age != undefined) person.age = personToBeUpdated.age
+    if (personToBeUpdated.name !== undefined) person.name = personToBeUpdated.name
+    if (personToBeUpdated.email !== undefined) person.email = personToBeUpdated.email
+    if (personToBeUpdated.age !== undefined) person.age = personToBeUpdated.age
     return person
   }
 
-  deletePersonRegistry = (id: string): Person | null => {
-    for (let i=0; i< this.personList.length; i++) {
-      if (this.personList[i].id === id) {
+  deletePersonBy = async (parameter: string, parameterValue: string): Promise<DatabasePerson | null> => {
+    for (let i = 0; i < this.personList.length; i++) {
+      if (this.personList[i].id === parameterValue) {
         const person = this.personList[i]
         this.personList.splice(i, 1)
         return person
